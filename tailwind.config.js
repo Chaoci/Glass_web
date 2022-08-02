83948b18439ea3f19aff618c3709a694a08f9795
Chaoci <charlieischao@gmail.com>
2022-08-02 12:13:35 +0800

    0802

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,22 @@
 module.exports = {
   content: ["./app/**/*.{html,ejs}"],
   theme: {
-    extend: {},
+    container:{
+      center:true,
+      padding:'12px', //只會設定左右 
+      screens: {
+        xl: '1320px',
+      },
+    },
+    extend: {
+      colors:{
+        'maroon':{
+            light: '#AA0601',
+            DEFAULT: '#650300',
+        },
+        'skin':'#FBF2F2',
+      }
+    },
   },
   plugins: [
     require('@tailwindcss/forms'),
